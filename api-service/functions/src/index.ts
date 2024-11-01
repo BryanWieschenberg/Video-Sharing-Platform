86c80c88ce7fdbd30758425f0529ca50f6eff904
Bryan Wieschenberg <wieschb1@tcnj.edu>
2024-11-01 18:02:19 -0400

    Added upload video feature to the api service

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -3,18 +3,47 @@ import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 // import {initializeApp} from "firebase-admin";
 // import {Firestore} from "firebase-admin/firestore";
+import { onCall } from "firebase-functions/v2/https";
 
 admin.initializeApp();
 
 const firestore = admin.firestore();
+const storage = admin.storage();
+const rawVideoBucketName = "bnw-raw-videos";
 
 export const createUser = functions.region("us-west1").auth.user().onCreate((user) => {
-  const userInfo = {
-    uid: user.uid,
-    email: user.email,
-    photoUrl: user.photoURL,
-  };
-  firestore.collection("users").doc(user.uid).set(userInfo);
-  logger.info(`User created: ${JSON.stringify(userInfo)}`);
-  return Promise.resolve();
+    const userInfo = {
+        uid: user.uid,
+        email: user.email,
+        photoUrl: user.photoURL,
+    };
+    firestore.collection("users").doc(user.uid).set(userInfo);
+    logger.info(`User created: ${JSON.stringify(userInfo)}`);
+    return Promise.resolve();
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+    // Check if the user's authenticated
+    if (!request.auth) {
+        throw new functions.https.HttpsError (
+            "failed-precondition",
+            "The function must be called while authenticated."
+        );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique file name
+    const fileName = `${auth.uid}-${Date.now()}-${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+        version: "v4",
+        action: "write",
+        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {url, fileName};
 });
