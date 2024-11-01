9d78615314759a4d3cbaf1b671266e20653815ea
Bryan Wieschenberg <wieschb1@tcnj.edu>
2024-10-31 20:48:42 -0400

    Created clientside base page and navbar

--- web-client/app/navbar/navbar.tsx
+++ web-client/app/navbar/navbar.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
     return (
         <nav>
-            <Image src="/logo.svg" alt="Logo" />
+            <Link href="/">
+                <Image width={90} height={20}
+                    src="/logo.svg" alt="Logo" />
+            </Link>
         </nav>
     );
 }
